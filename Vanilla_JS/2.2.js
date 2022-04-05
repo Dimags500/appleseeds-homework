546b74774ee329af3a344236e6f563e332423be7
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-04-05 21:49:26 +0300

    2.2 functions

---
+++ Vanilla_JS/2.2.js
@@ -0,0 +1,5 @@
+function tellFortune(jobTitle, location, partnerName, numberOfChildren) {
+  return ` some day ${jobTitle} in ${location}  with ${partnerName} ..... ${numberOfChildren} times  `;
+}
+
+console.log(tellFortune("rock", "Taganrog", "Riki", 2000000));
