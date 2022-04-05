fe1848da6e20f515515722ee731213a2bf4688ed
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-04-05 13:21:41 +0300

    folder for js added

---
+++ Vanilla_JS/1.1.js
@@ -0,0 +1,15 @@
+let srt = 'Rock wil "Win"';
+let num = (10 % 6) ** 2;
+let bool = false;
+let obj = new Object();
+let someNull = null;
+const someUndifind = undefined;
+let someSymbol = Symbol();
+
+console.log(srt);
+console.log(num);
+console.log(bool);
+console.log(obj);
+console.log(someNull);
+console.log(someUndifind);
+console.log(someSymbol);
