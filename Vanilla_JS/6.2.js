39e268fae7ce75e37ad4549bf727ddff4297f034
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-04-10 14:44:05 +0300

    6.2 slice slice concat for

---
+++ Vanilla_JS/6.2.js
@@ -0,0 +1,52 @@
+const people = ["Greg", "Mary", "Devon", "James"];
+
+//------------------------------------------------ 1,2
+// people.shift();
+// people.pop();
+
+// for (let i = 0; i < people.length; i++) {
+//   console.log(people[i]);
+// }
+
+//------------------------------------------------------- 4,5
+
+// people.unshift("Matt");
+// people.push("Nick");
+
+// for (let i = 0; i < people.length; i++) {
+//   console.log(people[i]);
+// }
+
+// ---------------------------------------------------  6
+
+// for (let i = 0; i < people.length; i++) {
+//   console.log(people[i]);
+//   if (people[i] == "Mary") {
+//     break;
+//   }
+// }
+
+//------------------------------------------------------  7
+
+// const new_arr = people.slice(2);
+
+// for (let i = 0; i < new_arr.length; i++) {
+//   console.log(new_arr[i]);
+// }
+
+//--------------------------------------------------------- 8 , 9
+
+// console.log(people.indexOf("Mary"));
+
+// console.log(people.indexOf("Foo"));
+
+//--------------------------------------------------------- 10
+
+// people.splice(2, 1);
+// people.splice(2, 0, "Elizabeth", "Artie");
+// console.log(people);
+
+//------------------------------------------------------------- 11
+
+// let withBob = people.concat("Bob");
+// console.log(withBob);
