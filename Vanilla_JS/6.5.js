8219f9e5b21be4405e1e073e5fbe5524478e6182
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-04-11 10:08:18 +0300

    6.5 done

---
+++ Vanilla_JS/6.5.js
@@ -0,0 +1,47 @@
+var arry = new Array(100);
+arry.fill("Filled");
+
+// ---------------------------------------------2
+var arry2 = new Array(100);
+let arr3 = Array.from(arry2, (v, i) => i);
+
+//--------------------------------------------- 3
+const obj = { 1: "val1", 2: "val2", 3: "val 3 " };
+const obj_arr = Object.values(obj);
+
+// ---------------------------------------------4
+
+const arr1 = [12, 3, 55, 4];
+
+const obj_to_arr = Object.assign({}, arr1);
+
+// --------------------------------------------- 5
+
+Array.isArray(obj_to_arr);
+
+// -------------------------------------- 6 , 7
+
+const arr2 = [12, 3, 55, 4];
+
+// Shallow copy 1
+
+// const clone_arr = arr2;
+// console.log(clone_arr + "-------   Shallow copy ,befor new item ");
+// clone_arr.push(100);
+// console.log(arr2 + "------- Shallow copy ,after new item  ");
+
+// Shallow copy 2
+
+// const clone_arr1 = [...arr2];
+// console.log(arr2 + "-------   Shallow copy ,befor new item ");
+// clone_arr1.push(100);
+// console.log(arr2 + "------- Shallow copy ,after new item  ");
+
+//Deep copy
+
+// const clone_arr2 = JSON.parse(JSON.stringify(arr2));
+// console.log(clone_arr2 + "------- Deep copy ,befor new item IN arr2");
+// arr2.push(3000);
+// console.log(
+//   clone_arr2 + "------- Deep copy ,after new item in arr2 (no 3000) "
+// );
