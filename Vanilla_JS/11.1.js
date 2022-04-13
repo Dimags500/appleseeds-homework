d3aa001d3e4bcce49ab5bc39fcae669c7d6bc0cc
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-04-13 10:47:59 +0300

    11.1 map&foreach

---
+++ Vanilla_JS/11.1.js
@@ -0,0 +1,51 @@
+const newReleases = [
+  {
+    id: 70111470,
+    title: "Die Hard",
+    boxart: "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+    uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+    rating: [4.0],
+    bookmark: [],
+  },
+  {
+    id: 654356453,
+    title: "Bad Boys",
+    boxart: "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+    uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+    rating: [5.0],
+    bookmark: [{ id: 432534, time: 65876586 }],
+  },
+  {
+    id: 65432445,
+    title: "The Chamber",
+    boxart: "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+    uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+    rating: [4.0],
+    bookmark: [],
+  },
+  {
+    id: 675465,
+    title: "Fracture",
+    boxart: "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+    uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+    rating: [5.0],
+    bookmark: [{ id: 432534, time: 65876586 }],
+  },
+];
+
+//------------------------------------------------------ Foreach
+
+// const foreach_arr = [];
+// newReleases.forEach((x) => {
+//   foreach_arr.push({ id: x.id, name: x.title });
+// });
+
+// console.log(foreach_arr);
+
+//---------------------------------------------------------- Map
+
+// const map_arr = newReleases.map((item) => {
+//   return { id: item.id, name: item.title };
+// });
+
+// console.log(map_arr);
