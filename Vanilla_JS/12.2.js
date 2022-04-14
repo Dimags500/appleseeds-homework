ade249e9b78d48e7a4ea6ca3e4e719815836f803
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-04-14 09:40:28 +0300

    12.2 candyStore

---
+++ Vanilla_JS/12.2.js
@@ -0,0 +1,56 @@
+const candyStore = {
+  candies: [
+    {
+      name: "mint gum",
+      id: "as12f",
+      price: 2,
+      amount: 2,
+    },
+    {
+      name: "twix",
+      id: "5hd7y",
+      price: 5,
+      amount: 4,
+    },
+  ],
+  cashRegister: 200,
+};
+
+// ----------------------------------------------  1
+
+const getCandy = (candyStore, id) =>
+  candyStore.candies.find((item) => item.id == id);
+
+// const id_result = getPrice(candyStore, "5hd7y");
+// console.log(id_result);
+
+//--------------------------------------------------2
+
+// const getPrice = (candyStore, id) =>
+//   candyStore.candies.find((item) => item.id == id).price;
+
+// const price_result = getPrice(candyStore, "5hd7y");
+// console.log(price_result);
+
+//-------------------------------------------------- 3
+
+// function addCandy(candyStore, id, name, price) {
+//   candyStore.candies.push({ name: name, id: id, price: price || 1 });
+// }
+
+// addCandy(candyStore, "r3r3", "rock", 20);
+// console.log(candyStore.candies);
+
+//--------------------------------------------------- 4
+
+function buy(candyStore, id) {
+  const candy = getCandy(candyStore, id);
+
+  candy.amount -= 1;
+
+  candyStore.cashRegister += candy.price;
+}
+console.log(candyStore);
+
+buy(candyStore, "5hd7y");
+console.log(candyStore);
