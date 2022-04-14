12e0fe1a7c7df46d5648655dfd19483b535eaae4
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-04-14 17:55:58 +0300

    12.3 filter&find methods

--- Vanilla_JS/12.3.js
+++ Vanilla_JS/12.3.js
@@ -39,35 +39,96 @@ const school = {
   ],
 };
 
-// ------------------------------------------ 1
+// ----------------------------------------------------------------- 1
 
-function findPerson(type, id) {
-  if (type == "teacher") {
-    const teacher = school.teachers.find((t) => t.id == id);
-    return teacher !== undefined ? teacher : "Not exsist";
-  }
-  if (type == "student") {
-    const student = school.students.find((s) => s.id == id);
-    return student !== undefined ? student : "Not exsist";
-  } else return "Wrong type";
-}
+// function findPerson(type, id) {
+//   if (type == "teacher") {
+//     const teacher = school.teachers.find((t) => t.id == id);
+//     return teacher !== undefined ? teacher : "Not exsist";
+//   }
+//   if (type == "student") {
+//     const student = school.students.find((s) => s.id == id);
+//     return student !== undefined ? student : "Not exsist";
+//   } else return "Wrong type";
+// }
 
 // console.log(findPerson("studefnt", 01));
 
-// ------------------------------------------ 2
+// ---------------------------------------------------------------- 2
 
-function assignStudent(student_id, subject) {
-  const teachers = school.teachers
-    .map((x) => x.subjects)
-    .join()
-    .split(",");
+// function assignStudent(student_id, subject) {
+//   let available_teachers = school.teachers.filter((t) => t.capacityLeft > 0);
 
-  console.log(teachers);
-}
+//   let teacher_with_required_subject = available_teachers.find((s) =>
+//     s.subjects.includes(subject)
+//   );
 
-assignStudent(12, "ethics");
+//   if (
+//     available_teachers === undefined ||
+//     available_teachers.length < 1 ||
+//     teacher_with_required_subject === undefined ||
+//     teacher_with_required_subject.length < 1
+//   ) {
+//     console.log("Sorry, no available teachers left for " + subject);
 
-// first available teacher who teaches that subject --- find in subjects
-//  and who is not in full                          ---- capasityleft >= students.len
+//     return;
+//   }
 
-// ------------------------------------------ 3
+//   if (teacher_with_required_subject.capacityLeft >= 1) {
+//     teacher_with_required_subject.capacityLeft--;
+//     teacher_with_required_subject.students.push(student_id);
+//     console.log(teacher_with_required_subject);
+
+//     if (teacher_with_required_subject.capacityLeft == 0) {
+//       console.log(`The ${subject} class is full`);
+//       return;
+//     }
+//   }
+// }
+
+// assignStudent(12, "ethics");
+// console.log("----------------------------");
+
+// assignStudent(11, "ethics");
+// console.log("----------------------------");
+// assignStudent(13, "biology");
+
+// console.log("----------------------------");
+// assignStudent(10, "biology");
+
+// console.log("----------------------------");
+// assignStudent(11, "ethics");
+
+// console.log("----------------------------");
+
+// assignStudent(13, "biology");
+
+// console.log("----------------------------");
+// assignStudent(10, "physics");
+
+// console.log("----------------------------");
+// assignStudent(11, "physics");
+
+// -------------------------------------------------------- 3
+
+// function assignTeachersSubject(teacher_id, new_subject) {
+//   const teacher = school.teachers.find((t) => t.id == teacher_id);
+
+//   if (!teacher.subjects.includes(new_subject)) {
+//     teacher.subjects.push(new_subject);
+//   }
+//   console.log(teacher);
+// }
+
+// assignTeachersSubject(2, "history");
+
+// -------------------------------------------------------- 4
+
+// function getAllStudentsWithNameStartsWithHorJ(data) {
+//   return data.students.filter(
+//     (s) => s.name.startsWith("H") || s.name.startsWith("J")
+//   );
+// }
+
+// const students = getAllStudentsWithNameStartsWithHorJ(school);
+// console.log(students);
