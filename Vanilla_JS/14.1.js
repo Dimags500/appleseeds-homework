f33627447ea6c99c2b2381aa2658b899a8ad7c05
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-04-17 10:54:28 +0300

    14.1 reduce method

---
+++ Vanilla_JS/14.1.js
@@ -0,0 +1,31 @@
+// Write the following functions using the reduce built-in function.
+// 1. max
+// 2. sum of even numbers
+// 3. average
+
+const array = [20, 100, 32, 10, 2];
+//------------------------------------------------- 1
+
+// const max = array.reduce((max, curr) => {
+//   if (max < curr) return curr;
+
+//   return max;
+// });
+// console.log(max);
+
+//------------------------------------------------- 2
+
+// const sumOfEven = [10, 3, 5, 7, 9].reduce((sum, curr) => {
+//   if (curr % 2 == 0) {
+//     sum += curr;
+//   }
+
+//   return sum;
+// });
+
+// console.log(sumOfEven);
+
+//------------------------------------------------- 3
+
+// const average = array.reduce((avg, curr, i) => (avg += curr)) / array.length;
+// console.log(average);
