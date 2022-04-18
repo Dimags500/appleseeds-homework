8963d516f45950b866962923328684a318d73dc2
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-04-18 22:52:09 +0300

    17.1. closure

---
+++ Vanilla_JS/17.1.js
@@ -0,0 +1,40 @@
+// ----------------------------------------   block 1
+
+// var b = 1;
+// function someFunction(number) {
+//   function otherFunction(input) {
+//     return b;
+//   }
+//   b = 5;
+//   return otherFunction;
+// }
+// var firstResult = someFunction(9); // 5
+// var result = firstResult(2); // 5
+
+// console.log(result);
+
+// firstResult runs someFunction() and result is returned otherFunction()
+// otherFunction() has closure member that store parrent values that ths function is thepends on
+
+// ----------------------------------------   block 2
+
+var a = 1;
+function b2() {
+  a = 10;
+  return;
+  function a() {}
+}
+b2();
+console.log(a);
+
+// in b2 function frist we put value 10 in var a , but after we overide to local funtion a
+
+// ----------------------------------------   block 3
+
+// let i;
+// for (i = 0; i < 3; i++) {
+//  const log = () => {
+//  console.log(i);
+//  }
+//  setTimeout(log, 100);
+// }
