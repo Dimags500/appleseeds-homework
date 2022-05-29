f5a3d486c553c877b7fb16fd85970e3412b85ae4
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-05-29 19:44:37 +0300

    18.2  useState

--- react/18/hooks/src/App.jsx
+++ react/18/hooks/src/App.jsx
@@ -4,26 +4,26 @@ import { Main } from "./18.2/Main";
 
 
 
-export const App = ()=>{
-
-    return(
-        <>
-        <Text text='lern Big big ' num="5" / >
-            <>--------</>
-            <Text text='lern as sssd f45545657 343434343434' num="10" / >
-
-            </>
-    ) 
-   
-}
-
-
 // export const App = ()=>{
 
 //     return(
 //         <>
-//        <Main />
+//         <Text text='lern Big big ' num="5" / >
+//             <>--------</>
+//             <Text text='lern as sssd f45545657 343434343434' num="10" / >
+
 //             </>
-//     ) ;
+//     ) 
    
 // }
+
+
+export const App = ()=>{
+
+    return(
+        <>
+       <Main />
+            </>
+    ) ;
+   
+}
